74cccd963749171319ada42420394e7c671d2583
성이름 <suno.vivid@gmail.com>
2020-11-30 14:57:41 +0000

    Move 2609 unsolved

---
+++ unsolved/2609.cpp
@@ -0,0 +1,46 @@
+//TODO: 더 개선된 방법 존재
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <assert.h>
+#include <string.h>
+#include <string>
+
+#define MAX
+
+using namespace std;
+
+/* 문제 정의
+ n, m -> 최대공약수, 최소공배수
+ */
+
+/* 해결 알고리즘 생각
+gcd(a,b) = (가능한 경우) gcd(a/i, b/i) * i 또는 (불가능한 경우) 1
+*/
+
+/* 시간복잡도
+
+*/
+int gcd(int a, int b) {
+	for (int i=2;i<=min(a,b);i++)
+		if (a % i == 0 && b % i == 0)
+			return gcd(a/i, b/i) * i;
+	return 1;
+}
+
+int main()
+{
+    /////////////input&preprocessing/////////////
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+	int n, m;
+	cin >> n >> m;
+    ///////////////////solution//////////////////
+	int gcd_temp = gcd(n, m);
+	cout << gcd_temp << '\n' << (n * m) / gcd_temp;
+    ////////////////////output///////////////////
+
+    return 0;
+}
