e5e8389f756cb9ee7dd31f9d3c15cf4f7c8bc116
sunovivid <suno.vivid@gmail.com>
2022-05-23 15:36:36 +0900

    Solve 17626 Four Squares

--- Baekjoon Online Judge/Four_Squares.py
+++ Baekjoon Online Judge/Four_Squares.py
@@ -1,17 +1,17 @@
 # 17626 Four Squares
 import itertools
 
-MAX = 50_000
+MAX = 100_000
 
 
 def answer(n):
-    squares = [i * i for i in range(1, int(n ** 0.5) + 1) if i * i < MAX]
+    squares = set((i * i for i in range(1, int(n ** 0.5) + 1) if i * i < MAX))
     if n in squares:
         return 1
     for x in squares:
         if n - x in squares:
             return 2
-    for x, y in itertools.combinations(squares, 2):
+    for x, y in itertools.combinations_with_replacement(squares, 2):
         if n - (x + y) in squares:
             return 3
     return 4
@@ -19,3 +19,7 @@ def answer(n):
 
 n = int(input())
 print(answer(n))
+
+'''
+93% 틀렸습니다 - 같은 숫자가 중복되는 경우 고려 X -> combinations_with_replacement로 수정
+'''
